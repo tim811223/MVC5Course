82dc2fd09c913b2739f43b82ba8c5554a9d3639d
Will <doggy.huang@gmail.com>
2017-12-02 16:22:37 +0800

    08 建立新的 TestController 並練習透過 Entity Framework 操作 CRUD 動作 ( 50% )

---
+++ MVC5Course/Controllers/TestController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class TestController : Controller
+    {
+        FabricsEntities db = new FabricsEntities();
+
+        public ActionResult Index()
+        {
+            var data = from p in db.Product
+                       select p;
+
+            return View(data.Take(10));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create(Product data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(data);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(data);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.Product.Find(id);
+            return View(item);
+        }
+    }
+}
--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,8 +11,7 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -22,15 +21,9 @@ namespace MVC5Course.Models
         }
     
         public int ProductId { get; set; }
-        [Required(ErrorMessage = "請輸入商品名稱")]
         public string ProductName { get; set; }
-        [Required]
-        [Range(0, 999, ErrorMessage = "商品金額只能介於 0 ~ 999 之間")]
         public Nullable<decimal> Price { get; set; }
-        [Required]
         public Nullable<bool> Active { get; set; }
-        [Required]
-        [DisplayFormat(DataFormatString = "{0:#}")]
         public Nullable<decimal> Stock { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
