497f171f2b90c61e7d8611b4981b86f7979bb406
Will <doggy.huang@gmail.com>
2017-12-03 11:56:07 +0800

    12 練習透過 Repository 與 UnitOfWork 來存取所有資料

--- MVC5Course/Models/ProductRepository.cs
+++ MVC5Course/Models/ProductRepository.cs
@@ -6,6 +6,10 @@ namespace MVC5Course.Models
 {   
 	public  class ProductRepository : EFRepository<Product>, IProductRepository
 	{
+        public override IQueryable<Product> All()
+        {
+            return base.All().Where(p => p.IsDeleted == false);
+        }
         public Product Find(int id)
         {
             return this.All().FirstOrDefault(p => p.ProductId == id);
@@ -13,11 +17,11 @@ namespace MVC5Course.Models
 
         public IQueryable<Product> Get取得所有尚未刪除的商品資料()
         {
-            return this.All().Where(p => p.IsDeleted == false);
+            return this.All();
         }
         public IQueryable<Product> Get取得所有尚未刪除的商品資料Top10()
         {
-            return this.All().Where(p => p.IsDeleted == false).Take(10);
+            return this.All().Take(10);
         }
     }
 
