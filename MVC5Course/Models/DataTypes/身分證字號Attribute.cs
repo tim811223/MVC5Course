dd92292d4aa7309ca9118b082621ca7aa2b86347
Will <doggy.huang@gmail.com>
2017-12-03 10:41:44 +0800

    11 練習自訂輸入驗證與模型驗證

---
+++ MVC5Course/Models/DataTypes/身分證字號Attribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models.DataTypes
+{
+    public class 身分證字號Attribute : DataTypeAttribute
+    {
+        public 身分證字號Attribute() : base(DataType.Text)
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            string str = (string)value;
+
+            if (str.Contains("Will"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -1,20 +1,29 @@
 namespace MVC5Course.Models
 {
+    using MVC5Course.Models.DataTypes;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ProductMetaData))]
-    public partial class Product
+    public partial class Product : IValidatableObject
     {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.Stock < 10 && this.Price > 1000)
+            {
+                yield return new ValidationResult("Stock < 10 && Price > 1000 是錯誤的資料設定！", new string[] { "Stock", "Price" });
+            }
+        }
     }
-    
+
     public partial class ProductMetaData
     {
         [Required]
         public int ProductId { get; set; }
         
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [身分證字號]
         public string ProductName { get; set; }
         [Required]
         public Nullable<decimal> Price { get; set; }
