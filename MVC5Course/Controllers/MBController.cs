36a936e0bed084403532ef4d6b5609abd227bdc2
Will <doggy.huang@gmail.com>
2017-12-16 09:51:55 +0800

    實作多筆資料更新功能 (View 部分尚未完成)

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -6,17 +6,43 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
+    public class MBBatchUpdateVM
+    {
+        public int ProductId { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public Nullable<decimal> Stock { get; set; }
+    }
     public class MBController : BaseController
     {
         public ActionResult Index()
         {
             var data = repo.Get取得所有尚未刪除的商品資料Top10();
-
             ViewData.Model = data;
+            ViewBag.PageTitle = "商品清單";
+            return View();
+        }
 
-            ViewData["PageTitle"] = "商品清單";
-            //ViewBag.PageTitle = "商品清單";
+        [HttpPost]
+        public ActionResult Index(MBBatchUpdateVM[] batch)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var item in batch)
+                {
+                    var one = repo.Find(item.ProductId);
+                    one.Price = item.Price;
+                    one.Active = item.Active;
+                    one.Stock = item.Stock;
+                }
+                repo.UnitOfWork.Commit();
 
+                return RedirectToAction("Index");
+            }
+
+            var data = repo.Get取得所有尚未刪除的商品資料Top10();
+            ViewData.Model = data;
+            ViewBag.PageTitle = "商品清單";
             return View();
         }
     }
