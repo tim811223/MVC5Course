7215703ddd791a9efd93f795b1f8602140b6c5ab
Will <doggy.huang@gmail.com>
2017-12-17 16:51:34 +0800

    31 練習將 DateOfBirth 改用 EditorTemplate 來呈現 jQuery UI 的 datepicker 效果

--- MVC5Course/Models/Client.Partial.cs
+++ MVC5Course/Models/Client.Partial.cs
@@ -26,6 +26,7 @@ namespace MVC5Course.Models
         [StringLength(1, ErrorMessage="欄位長度不得大於 1 個字元")]
         [UIHint("Gender")]
         public string Gender { get; set; }
+        [UIHint("DateOfBirth")]
         public Nullable<System.DateTime> DateOfBirth { get; set; }
         [UIHint("CreditRating")]
         public Nullable<double> CreditRating { get; set; }
